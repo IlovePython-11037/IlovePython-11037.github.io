b7380cb71e3371c7287736dea7801b6df589cb38
IlovePython-11037 <magee54177@students.apsb.org>
2025-04-14 13:10:42 +0000

    Finished sorting

--- asd-projects/sorting/index.js
+++ asd-projects/sorting/index.js
@@ -15,20 +15,53 @@ The CSS ids you will work with are:
 
 // TODO 2: Implement bubbleSort
 async function bubbleSort(array){
-    let sorted = false
-    for(let i = 0; i < array.length -n1; i++){
-        for(let j = 0; j < array.length -1; j++){
-            if(array[j] < array[j]) 
-
+    for(i = 0; i < array.length; i++){
+        for(j = array.length - 1; j > i; j--){
+            if(array[j].value < array[j - 1].value){
+                swap(array, j, (j - 1));
+                updateCounter(bubbleCounter);
+                await sleep();
+            }
         }
     }
+
 }
 
 // TODO 3: Implement quickSort
-
+async function quickSort(array, left, right){
+    if((right - left) > 0){
+        let index = await partition(array, left, right);
+        if(left < (index - 1)){
+           await quickSort(array, left, index - 1);
+            }
+        if(index < right){
+            await quickSort(array, index, right);
+            
+        }
+    }
+    else{
+        return
+    }
+}
 
 // TODOs 4 & 5: Implement partition
-
+async function partition(array, left, right){
+    let piviot = array[Math.floor((right + left)/2)].value;
+    while(left < right){
+        while(array[left].value < piviot){
+            left++
+        }
+        while(array[right].value > piviot){
+            right--
+        }
+        if(left < right){
+            swap(array, left, right);
+            updateCounter(quickCounter);
+            await sleep();
+        }
+    }
+    return left + 1
+}
 
 // TODO 1: Implement swap
 function swap(array, i , j){
